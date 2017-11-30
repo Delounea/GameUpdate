98acac66ad9bdb2cad00f22b031afa86f879a1fa
Delounea <martin_martin1993@hotmail.com>
2017-11-30 23:20:39 +0100

    Remove pets route

--- server/routes.js
+++ server/routes.js
@@ -2,13 +2,11 @@ const Router = require('express').Router;
 const router = new Router();
 
 const user = require('./model/user/router');
-const pet = require('./model/pet/router');
 
 router.route('/').get((req, res) => {
   res.json({ message: 'Welcome to game-update-server API!' });
 });
 
 router.use('/user', user);
-router.use('/pet', pet);
 
 module.exports = router;
