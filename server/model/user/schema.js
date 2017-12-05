921d79c87c9a3a55c33d4e9edb71542b35b4b0ce
Delounea <martin_martin1993@hotmail.com>
2017-12-05 21:23:36 +0100

    Add schema for user

--- server/model/user/schema.js
+++ server/model/user/schema.js
@@ -3,8 +3,13 @@ const Schema = mongoose.Schema;
 
 
 const userSchema = new Schema({
-  title: { type: String, required: true },
-  body: { type: String }
+  username: { type: String, required: [true, 'Username required'] },
+  password: { type: String, required: [true, 'Password required'] },
+  mail: {
+    type: String,
+    validator: v => /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v),
+    required: [true, 'Email required']
+  }
 });
 
 
