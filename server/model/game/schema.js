d18da509514c7271664683dfa2896337946cec6c
Delounea <martin_martin1993@hotmail.com>
2017-12-17 01:08:56 +0100

    Change gameSchema

--- server/model/game/schema.js
+++ server/model/game/schema.js
@@ -3,11 +3,14 @@ const Schema = mongoose.Schema;
 
 
 const gameSchema = new Schema({
-  label : {},
-  lastVersion: {},
-  lastVersionDate: {},
-  steamBuildID: {}
+  label : { type: String, index: { unique: true }, required: [true, 'Label required'] },
+  versionDate: { type: Date, default: Date.now },
+  version: { type: String, required: [true, 'Version required'] },
+  gamePlatform: { type: String, required: [true, 'Game platform required'] },
+  updateSizes: [{
+    version : String,
+    size: Number
+  }]
 });
-
 
 module.exports =  gameSchema;
